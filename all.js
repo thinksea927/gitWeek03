eef46c45d1d385dedb27558d856e94babeb28d7b
siling <think.sea@gmail.com>
2020-09-21 13:36:56 +0800

    add js feature2

--- all.js
+++ all.js
@@ -1,5 +1,13 @@
+// 九九乘法(從3開始)
 for(let i=3; i<16; i++){
   for(let j=1; j<16; j++){
     console.log(i+"x"+j+"="+i*j);
   }
+}
+
+// BMI counter
+function input(height, weight){
+
+  console.log((weight/(height/100*height/100)).toFixed(2));
+
 }
